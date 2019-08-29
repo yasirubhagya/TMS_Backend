8f317b5ab67fc2af9200999b35a9b8a786456fd6
yasirubhagya <yasirubhagya@gmail.com>
2019-08-29 09:45:08 +0530

    saving data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://savy:iD5MG1YKgBc1APaX@cluster0-bsqiz.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//const MongoClient = require('mongodb').MongoClient;
+//const uri = "mongodb+srv://savy:iD5MG1YKgBc1APaX@cluster0-bsqiz.mongodb.net/test?retryWrites=true&w=majority";
+//const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 /* client.connect()
     .then(result => {
         return result.db("test").createCollection("ABC123");
@@ -16,19 +16,35 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
     .catch(err => {
         console.log(err);
     }); */
-
+const mongoose = require('mongoose');
+const gpsImuData = require('./gpsImuModel');
+mongoose.connect('mongodb+srv://savy:O5Y0j2Ml4ZzEbhJ4@cluster0-bsqiz.mongodb.net/savyDB?retryWrites=true&w=majority',
+    { useNewUrlParser: true }
+);
+mongoose.Promise = global.Promise;
 var mqtt = require('mqtt');
-var mqttclient = mqtt.connect('mqtt://localhost:1883', { username: 'guagtchv', password: '7x4D_P5Hjiet', clientId: 'sc01', clean: false });
-
+var mqttclient = mqtt.connect('mqtt://broker.hivemq.com:1883', { username: 'guagtchv', password: '7x4D_P5Hjiet', clientId: 'sc01', clean: true });
+let client_Id = 'Tr0'
 mqttclient.on('connect', function (connack) {
     if (connack.sessionPresent) {
         console.log("previous session is still available");
-    } else {
-        mqttclient.subscribe('presence/#', function (err) {
-            if (!err) {
-                mqttclient.publish('presence', 'Hello mqtt')
+    }
+    else {
+
+        mqttclient.subscribe(
+            {
+                [`savyMqtt/trackers/${client_Id}/data`]: { qos: 1 },
+                [`savyMqtt/trackers/${client_Id}/usd/result`]: { qos: 1 },
+                [`savyMqtt/trackers/${client_Id}/siminfo/result`]: { qos: 1 },
             }
-        })
+            , function (err) {
+                if (!err) {
+                    console.log("subscribed success");
+                }
+                else {
+                    console.log(err);
+                }
+            });
     }
 })
 
@@ -38,7 +54,38 @@ mqttclient.on('error', (err) => {
 
 mqttclient.on('message', function (topic, message) {
     // message is Buffer
-    console.log(message.toString())
     //mqttclient.end()
-})
+    if (topic === `savyMqtt/trackers/${client_Id}/data`) {
+            
+        let m =JSON.parse(message);
+        console.log(m);
+        new gpsImuData({
+            trackerId: m.trackerId,
+            dateTime: new Date(),
+            latitude: m.latitude,
+            longitude: m.longitude,
+            imu: [
+                {
+                    accelx: 2.5,
+                    accely: 5.6,
+                    accelz: 3.2,
+                    gyrox: 5.3,
+                    gyroy: 2.3,
+                    gyroz: 56.2
+                }
+            ]
+        })
+            .save()
+            .then(result => {
+                console.log("Entry saved")
+            })
+            .catch(error => {
+               // console.log(error);
+            })
+    }
 
+});
+
+setInterval(() => {
+    mqttclient.publish(`savyMqtt/trackers/${client_Id}/pull`, "pull");
+}, 5000)
---
+++ gpsImuModel.js
@@ -0,0 +1,35 @@
+
+
+const mongoose = require('mongoose');
+const gpsSchema = mongoose.Schema({
+    trackerId: {
+        type: String,
+        required: true
+    },
+    dateTime: {
+        type: Date,
+        required: true
+    },
+    latitude: {
+        type: Number,
+        required: true
+    },
+    longitude: {
+        type: Number,
+        required: true
+    },
+    imu: [
+        {
+            accelx: { type: Number },
+            accely: { type: Number },
+            accelz: { type: Number },
+            gyrox: { type: Number },
+            gyroy: { type: Number },
+            gyroz: { type: Number }
+        }
+    ]
+
+});
+
+
+module.exports = mongoose.model('GpsImuData', gpsSchema);
